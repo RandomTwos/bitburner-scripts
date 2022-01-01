a186fcb5b1bd931c3135d3e08911a02dfeda3945
random.twos <random.twos@gmail.com>
2022-01-01 10:45:42 -0600

    updated passesed numNodes

--- src/scripts/build-hacknet.js
+++ src/scripts/build-hacknet.js
@@ -6,7 +6,7 @@ export async function main(ns) {
     while (ns.hacknet.numNodes() < 12) {
         if (ns.getServerMoneyAvailable("home") > (ns.hacknet.getPurchaseNodeCost(1) * .75)){
             ns.hacknet.purchaseNode(1)
-            ns.run("/bin/hacknet-upgrade.js", 1, ns.hacknet.numNodes())
+            ns.run("/bin/hacknet-upgrade.js", 1, ns.hacknet.numNodes() - 1)
         }
         else {await ns.sleep(60000)}
     }
