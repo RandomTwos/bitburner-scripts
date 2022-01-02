633fadae6d960d63fb60667f46258b47fd9f5aab
random.twos <random.twos@gmail.com>
2022-01-01 22:00:07 -0600

    changed printed data to better reflect node being worked on

--- src/scripts/build-hacknet.js
+++ src/scripts/build-hacknet.js
@@ -2,12 +2,16 @@
 //import { NS } from '../../NetscriptDefinitions'
 export async function main(ns) {
     ns.disableLog('sleep')
+    const nodesWanted = ns.args[0]
+    let nodesPurchased = 0
 
-    while (ns.hacknet.numNodes() < 12) {
+    while (ns.hacknet.numNodes() < nodesWanted) {
         if (ns.getServerMoneyAvailable("home") > (ns.hacknet.getPurchaseNodeCost(1) * .75)){
             ns.hacknet.purchaseNode(1)
-            ns.run("/bin/hacknet-upgrade.js", 1, ns.hacknet.numNodes() - 1)
+            ns.tprint("PURCHASED NODE: " + nodesPurchased)
+            ns.run("/bin/hacknet-upgrade.js", 1, nodesPurchased)
+            nodesPurchased++
         }
-        else {await ns.sleep(60000)}
+        else {await ns.sleep(60e3)}
     }
 }
