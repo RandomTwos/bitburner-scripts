56fa3fb23acf719651c194bde063df3d4d5ce15d
random.twos <random.twos@gmail.com>
2022-01-02 09:49:56 -0600

    changed startup from running DAEMON to DAEMON running startup

--- src/scripts/DAEMON.js
+++ src/scripts/DAEMON.js
@@ -1,26 +1,57 @@
 /** @param {NS} ns **/
 //import { NS } from '../../NetscriptDefinitions'
+import { getRoot, threadCount } from '/lib/tools-lib.js'
 export async function main(ns) {
     ns.disableLog('ALL')
     ns.print("RUNNING DEAMON ENGINE")
 
     const portOpeners = ["BruteSSH.exe", "FTPCrack.exe", "relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe"]
+    const p1 = ns.getPortHandle(1)
 
     while (true) {
-        
-        // - if not joined factions at appropriate hack levels => popup
-        for (let f of ns.getPlayer().factions){
-            ns.print("FACTIONS JOINED: " + f)
+        // initialize Stage 1
+        if (!ns.scriptRunning("/bin/self-hack.js", "n00dles") && ns.getServerMoneyAvailable("home") < 1e6 ) {ns.run("/script/setup-stage1.js")}
+
+        // initialize Stage 2
+        if (!ns.scriptRunning("/bin/self-hack.js", "iron-gym") && ns.getServerMoneyAvailable("home") < 1e9 ) {ns.run("/script/setup-stage2.js")}
+
+        // check port(1) for servers that do not have self-hack on them, are valid targets
+        if (ns.peek(1) !== "“NULL PORT DATA”") {
+            let host = p1.pop()
+            if (getRoot(host)) { 
+                await ns.scp("/bin/self-hack.js", "home", host)
+                ns.exec("/bin/self-hack.js", host, threadCount(ns, "/bin/self-hack.js", host), host)
+                ns.print("SELF-HACK: " + host)
+             }
+            else {ns.tryWritePort(1, host)}
+            ns.print("FAILED ROOT: " + host)
         }
+
+
+        // reminders at specified hack levels
+        /*  switch( player hack level) {
+            case hacklevel: if not member of group spit out log reminder
+
+            100 CyberSec, 250, 400, 600, 2500 | for factions
+            50 portOpeners[1],  | for port openeners
+        */
+
+
         
         // - if not bought port openers at appropriate money || hack levels => toast
+        /*  switch( player hack level) {
+            case hacklevel: if not member of group spit out log reminder
+        */
+
+            /* case, based on hack level || money avaliable
         for (let open of portOpeners){ 
             if (ns.fileExists(open, "home") == false) {
                 ns.print("REMINDER - BUY " + open)
             }
         }
+        */
 
         // sleep to make loop work
-        await ns.sleep(600e3)
+        await ns.sleep(0)
     }
 }
