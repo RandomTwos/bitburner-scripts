361a0a39d0b6e573df8cfc1ffafd16e2cef0331a
random.twos <random.twos@gmail.com>
2022-01-01 21:58:50 -0600

    created to immediately attempt to root target server

---
+++ src/bin/root.js
@@ -0,0 +1,15 @@
+/** @param {NS} ns **/
+//import { NS } from '../../NetscriptDefinitions'
+export async function main(ns) {
+    const target = ns.args[0]
+    let p = 0
+
+    if (ns.fileExists("BruteSSH.exe", "home"))    {ns.brutessh(target); p++}
+    if (ns.fileExists("FTPCrack.exe", "home"))    {ns.ftpcrack(target); p++}
+    if (ns.fileExists("relaySMTP.exe", "home"))   {ns.relaysmtp(target); p++}
+    if (ns.fileExists("HTTPWorm.exe", "home"))    {ns.httpworm(target); p++}
+    if (ns.fileExists("SQLInject.exe", "home"))   {ns.sqlinject(target); p++}
+    
+    if (ns.getServerNumPortsRequired(target) > p) {ns.nuke(target)}
+    else {ns.tprint("NOT ENOUGH PORTS OPENED")}
+}
