6a0995742f668a5009008b0bd1afc6d059e18175
random.twos <random.twos@gmail.com>
2022-01-08 09:42:30 -0600

    updated

--- src/bin/host-prime.js
+++ src/bin/host-prime.js
@@ -1,13 +1,47 @@
 /** @param {NS} ns **/
 import { NS } from '../../NetscriptDefinitions'
+import { threadsWeaken, threadCount, usedRAM } from "/lib/lib-BN1.js"
 
 export async function main(ns:NS) {
     // calculate the number of weaken , grow , weaken to get server primed for managed hacking
-    let host = ns.augs[0]
+    let host = ns.args[0]
 
-    let weakenAnalyze = ns.weakenAnalyze(1)
-    let lvlSec = ns.getServerSecurityLevel(host)
-    let minSec = ns.getServerMinSecurityLevel(host)
+    let script = "/bin/host-weaken.js"
+    let threads = threadsWeaken(ns, host)
+    let neededRAM = usedRAM(ns, script, threads)
+    let hostRAM = ns.getServerMaxRam(host)
+    let maxRAMusage = neededRAM - hostRAM
 
-    ns.tprint(weakenAnalyze + " | " + lvlSec + " | " + minSec)
+    await ns.scp(script, "home", host)
+    if (maxRAMusage < 0) ns.exec(script, host, threads, host)
+    else {
+        while (maxRAMusage > 0 )
+
+    }
+    
+
+    while (neededRAM > 0) {
+        
+        ns.exec (script, host, )
+
+
+
+
+
+
+
 }
+
+/*
+
+copy script to host
+
+get script ram cost
+x = get number of times weaken can be ran on the server
+y = get number of times weaken is need to be  ran
+
+if y is greater than x 
+    figure out how many more time and run full batches then the remainder
+
+
+*/
